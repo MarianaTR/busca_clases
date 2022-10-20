b12fdb80a036261663935f81d079af11f3835897
MarianaTR <mdtorres1@miuandes.cl>
2022-10-20 08:28:23 -0300

    style login

--- web_project/Models/user.py
+++ web_project/Models/user.py
@@ -5,12 +5,20 @@ class users(db.Model):
     name = db.Column(db.String(50))
     email = db.Column(db.String(100))
     password = db.Column(db.String(100))
-    #clases = db.relationship('Clase')
+    phone = db.Column(db.String(100))
+    universidad = db.Column(db.String(100))
+    cargo = db.Column(db.String(100))
+    direcc = db.Column(db.String(100))
+    clases = db.relationship('Clase')
 
-    def __init__(self, name, email, password):
+    def __init__(self, name, email, password, phone, uni, cargo, dire):
         self.name = name
         self.email = email
         self.password = password
+        self.phone = phone
+        self.universidad = uni
+        self.cargo = cargo
+        self.direcc = dire
 
 
 def get_all_users():

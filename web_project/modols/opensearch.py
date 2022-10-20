5ca378de2cf500560075703a74f48319d64c3496
MarianaTR <mdtorres1@miuandes.cl>
2022-10-20 11:57:17 -0300

    implementacion de dockers

--- web_project/modols/opensearch.py
+++ web_project/modols/opensearch.py
@@ -1,6 +1,9 @@
 from opensearchpy import OpenSearch
 
-host = 'localhost'
+
+
+
+host = 'client'
 port = 9200
 #auth = ('admin', 'admin') # For testing only. Don't store credentials in code.
 #ca_certs_path = '/full/path/to/root-ca.pem' # Provide a CA bundle if you use intermediate CAs with your root CA.
@@ -10,19 +13,21 @@ port = 9200
 # client_key_path = '/full/path/to/client-key.pem'
 
 # Create the client with SSL/TLS enabled, but hostname verification disabled.
-
-client = OpenSearch(
-    hosts = [{'host': host, 'port': port}],
-    http_compress = True, # enables gzip compression for request bodies
-    #http_auth = auth,
-    # client_cert = client_cert_path,
-    # client_key = client_key_path,
-    use_ssl = False,
-    verify_certs = False,
-    #ssl_assert_hostname = False,
-    #ssl_show_warn = False,
-    #ca_certs = ca_certs_path
-)
+try :
+    client = OpenSearch(
+        hosts = [{'host': host, 'port': port}],
+        http_compress = True, # enables gzip compression for request bodies
+        #http_auth = auth,
+        # client_cert = client_cert_path,
+        # client_key = client_key_path,
+        use_ssl = False,
+        verify_certs = False,
+        #ssl_assert_hostname = False,
+        #ssl_show_warn = False,
+        #ca_certs = ca_certs_path
+    )
+except:
+    print("no se ha podido establecer conexion")
 
 
 index_name="iaps-index"

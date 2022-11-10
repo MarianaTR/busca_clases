24520269917e5f53270af424ac0ddc8e6b174398
cveralyon <cavera2@miuandes.cl>
2022-11-10 00:33:14 -0300

    casi ready

--- web_project/app.py
+++ web_project/app.py
@@ -9,25 +9,19 @@ from models import Clase, users
 from User import User
 import logging
 
+global_user = None
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = conection_db
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
-@app.route('/ping')
-def do_ping():
-    ping = 'Ping ...'
+@app.route('/logout', methods=["POST", "GET"])
+def logout():
+    global_user = None
+    return render_template("index.html")
 
-    response = ''
-    try:
-        response = requests.get('http://opensearch_node1:9200/iaps-index/_search')
-        print(response.text)
-    except requests.exceptions.RequestException as e:
-        print('\n Cannot reach the pong service.')
-        return 'Ping ...\n'
-
-    return 'Ping ... '+ response.text+ ' '+ str(type(response))+' \n'
 
 @app.route('/', methods=["POST","GET"])
 def index():
@@ -50,8 +44,9 @@ def index():
 def hello_world():  # put application's code here
     print("aqui")
 
-@app.route('/my_profile/<usr>')
-def profile(usr):
+
+@app.route('/my_profile', methods=["GET"])
+def profile(usr=global_user):
     return render_template("my_profile.html", usuario = usr)
 
 @app.route('/log_in', methods=["POST","GET"])
@@ -69,7 +64,7 @@ def login():
         if resp:
             user = User(resp[0], resp[1], resp[2], resp[3], 
                         resp[4], resp[5], resp[6], resp[7])
-            
+            global_user = user
             # Session['user'] = user
             return render_template("my_profile.html", usuario=user)
         else:
@@ -77,27 +72,34 @@ def login():
     else:
         return render_template("log_in.html")
 
-# session = Session()
-
-
 @app.route('/sign_in', methods=["POST","GET"])
 def regis():
+    logging.warning("antes de entrar a post")
     if request.method == 'POST' and (('password' and 'email' and 'nombre' and 'apellido' and 'celular' and 'cargo' and 'direcc') in request.form):
-        nombre = request.form['nombre'] + request.form['apellido']
+        logging.warning('entre al post')
+        nombre = request.form['nombre'] +" "+ request.form['apellido']
         mail = request.form['email']
         celular = request.form['celular']
         password = request.form['password']
-        universidad = request.form['universidad']
+        universidad = request.form['univer']
         cargo = request.form['cargo']
         direc = request.form['direcc']
-        user = users(nombre, mail, password, celular,
-                    universidad, cargo, direc)
+        
         #agregar a la base de datos un usuario
+        logging.warning("antes de insert:")
         resp = engine.connect().execute(
             'INSERT INTO users(name, email, password, phone, universidad, cargo, direcc) VALUES (%s, %s, %s, %s, %s, %s, %s)', nombre, mail, password, celular, universidad, cargo, direc)
+        resp = engine.connect().execute(
+            "SELECT * FROM users WHERE email = %s AND password = %s", mail, password)
+        resp = resp.fetchone()
         logging.warning("respuesta de bdd: %s", resp)
-            
-        return redirect(url_for("my_profile", usuario=user))
+        if resp:
+            user = User(resp[0], resp[1], resp[2], resp[3],
+                        resp[4], resp[5], resp[6], resp[7])
+            global_user = user
+            return render_template("my_profile.html", usuario=user)
+        else:
+            return render_template("sign_in.html")
     else:
         return render_template("sign_in.html")
 
